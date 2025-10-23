a033cd37020dd6b2c881b43f1afed834f2e14c36
Manuel Arzuaga <marzuagagarcia@gmail.com>
2025-10-23 16:26:28 -0300

    Segundo commit

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ const productSchema = new mongoose.Schema({
     precio:{type:Number,required:true},
     genero:{type:String,required:true},
     empresa:{type:String,required:true},
-    descripsion:{type:String,required:true},
+    descripcion:{type:String,required:true},
+    calificacion:{type:Number,required:true}
 
 },{
     versionKey:false
@@ -50,3 +51,70 @@ const authMiddleware = (req,res,next) =>{
     const decoded = jwt.verify(token,"Clave")
     next()
 }
+
+app.post("/auth/register",async (req,res)=>{
+    const body = req.body
+    const usuario = await Usuarios.findOne({email:body.email})
+
+    if(usuario){
+        return res.status(400).json({message:"El usuario ya existe"})
+    }
+
+    const hash = await bcrypt.hash(body.password,10)
+
+    const nuevoUsuario = new Usuarios({
+        email:body.email,
+        password:hash
+    })
+
+    await nuevoUsuario.save()
+
+    res.json(nuevoUsuario)
+})
+
+app.post("/auth/login",async (req,res)=>{
+    const body = req.body
+
+    const usuario = await Usuarios.findOne({email:body.email})
+
+    if(!usuario){
+        return res.status(401).json({status:"Usuario no encontrado"})
+    }
+
+    const passwordValidada = await bcrypt.compare(body.password,usuario.password)
+
+    if(!passwordValidada){
+        return res.status(401).json({status:"Usuario no encontrado"})
+    }
+
+    const token = jwt.sign({id:usuario.id,email:usuario.email},"Clave",{expiresIn:"1h"})
+    res.json({token})
+})
+
+app.get("/productos",authMiddleware,async(req,res)=>{
+    const producto = await Producto.find()
+    res.json({producto})
+})
+
+app.post("/productos",authMiddleware,async(req,res)=>{
+    const body = req.body
+    const {nombre,precio,genero,empresa,descripcion,calificacion} = body
+
+    if(!nombre || !precio || !genero || !empresa || !descripcion || !calificacion){
+        return res.status(400).json({status:"Datos invalidos"})
+    }
+
+    const nuevoProducto = new Producto({
+        nombre,
+        precio,
+        genero,
+        empresa,
+        descripcion,
+        calificacion
+    })
+
+    await nuevoProducto.save()
+    res.json(nuevoProducto)
+
+})
+
